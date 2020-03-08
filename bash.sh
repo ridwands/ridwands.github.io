ce77261b251f423907367e57a8270da81cddcc42
ridwands <37608486+ridwands@users.noreply.github.com>
2020-03-08 07:32:32 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir MidtransArchive
+sudo chmod 755 MidtransArchive
+counter=1
+while [ $counter -le 2 ]
+do
+touch MidtransArchive/Midtrans_Files$counter
+sudo chown vagrant:root MidtransArchive/*
+sudo chmod 775 MidtransArchive/*
+((counter++))
+done
+echo All Done
